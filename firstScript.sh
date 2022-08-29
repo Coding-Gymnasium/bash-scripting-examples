34b23f13e2454a735ff67104e119c6fb47a5d9d9
Nico Rithner <nicorithner@gmail.com>
2022-08-29 18:54:40 +0200

    Add comments and how to replace or append syntax

---
+++ firstScript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+<< 'NOTES'
+To make the file executable in the terminal run chmod +x <file name>
+to comment one line use #
+to make the file executable also add #!/bin/bash on the first line
+NOTES
+
+echo "my Favorite movie is \"Limitless\"" # display double quotes with backward slash and double quotes
+
+echo "Text pushed from firstScript.sh" > ./file.txt # the > replaces the content to append use >>
+
